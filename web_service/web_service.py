95ec0ca233acd1e7bc37d0b2ee6eb1ec1d5b50ae
mauroeren <ocakeren87@gmail.com>
2026-01-03 22:53:24 +0300

    Update HTML title in web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -9,7 +9,7 @@ HTML = """
 <!doctype html>
 <html>
 <head>
-    <title>Mikro Hizmetli Selam!</title>
+    <title>Merhaba Buluttan Selam!</title>
     <style>
         body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
         h1 { color: #333; }
